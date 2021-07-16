05511d8e126bad75dd17f4cb66a0cbf833ecab37
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-07-15 21:26:12 -0300

    feat: add validation rules in process transaction job

--- app/Jobs/ProcessTransactionJob.php
+++ app/Jobs/ProcessTransactionJob.php
@@ -2,16 +2,25 @@
 
 namespace App\Jobs;
 
+use App\Exceptions\NotEnoughtBalanceException;
+use App\Exceptions\PayerIsAShopKeeperException;
+use App\Exceptions\TransactionNotAuthorizedException;
+use App\Repositories\TransactionRepository;
+use App\Services\TransactionService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class ProcessTransactionJob implements ShouldQueue
 {
 
     use InteractsWithQueue, Queueable, SerializesModels;
+
+    private TransactionRepository $transactionRepository;
+    private TransactionService $transactionService;
 
     /**
      * Create a new job instance.
@@ -21,7 +30,8 @@ class ProcessTransactionJob implements ShouldQueue
     public function __construct(
         private array $transaction
     ) {
-        $this->transaction = $transaction;
+        $this->transactionRepository = app(TransactionRepository::class);
+        $this->transactionService = app(TransactionService::class);
     }
 
     /**
@@ -31,8 +41,28 @@ class ProcessTransactionJob implements ShouldQueue
      */
     public function handle()
     {
-        DB::table('transactions')
-            ->where('id', $this->transaction['id'])
-            ->update(['processed' => true]);
+        try {
+
+            if (!$this->transactionService->payerWalletHasEnoughBalance($this->transaction['payer_wallet_id'], $this->transaction['value'])) {
+                throw new NotEnoughtBalanceException();
+            }
+
+            if (!$this->transactionService->payerIsACommonUser($this->transaction['payer_wallet_id'])) {
+                throw new PayerIsAShopKeeperException();
+            }
+
+            if (!$this->transactionService->isTransactionAuthorized($this->transaction)) {
+                throw new TransactionNotAuthorizedException();
+            }
+
+            DB::beginTransaction();
+
+            $this->transactionRepository->updateBy($this->transaction['id'], ['processed' => true]);
+
+            DB::commit();
+        } catch (Throwable $e) {
+            dd($e);
+            throw $e;
+        }
     }
 }
