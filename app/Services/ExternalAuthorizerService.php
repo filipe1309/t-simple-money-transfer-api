a02fe347f78a16f119f6715350f47e6f677a2e64
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-07-15 21:22:17 -0300

    feat: add external authorizer service

---
+++ app/Services/ExternalAuthorizerService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+
+class ExternalAuthorizerService
+{
+    public function authorize(array $transaction): bool
+    {
+        $response = Http::post(env('EXTERNAL_AUTHORIZER_URL'), $transaction);
+
+        return $this->isAuthorized($response->json(), $response->status());
+    }
+
+    private function isAuthorized(array $responseBody, $responseStatus)
+    {
+        return $responseBody['message'] === env('EXTERNAL_AUTHORIZER_MESSAGE') && $responseStatus === Response::HTTP_OK;
+    }
+}
--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -5,34 +5,62 @@ namespace App\Services;
 use App\Events\NewTransactionCreatedEvent;
 use App\Jobs\ProcessTransactionJob;
 use App\Repositories\TransactionRepository;
+use App\Repositories\UserRepository;
+use App\Repositories\WalletRepository;
 use Ramsey\Uuid\Uuid;
 
 class TransactionService
 {
     public function __construct(
-        private TransactionRepository $repository
+        private TransactionRepository $transactionRepository,
+        private WalletRepository $walletRepository,
+        private UserRepository $userRepository,
+        private ExternalAuthorizerService $externalAuthorizerService
     ) {
     }
 
-    public function create(array $data): array
+    public function create(array $transaction): array
+    {
+        $transactionData = $this->prepareTransaction($transaction);
+
+        $transactionDatabaseResponse = $this->transactionRepository->create($transactionData);
+
+        dispatch(new ProcessTransactionJob($transactionDatabaseResponse))
+            ->onQueue('transactionJobQueue');
+
+        event(new NewTransactionCreatedEvent($transactionDatabaseResponse));
+
+        return $transactionDatabaseResponse;
+    }
+
+    private function prepareTransaction(array $transaction): array
     {
         $transaction_id = Uuid::uuid4()->toString();
 
-        $transaction = $this->repository->create(
-            [
-                'id' => $transaction_id,
-                'payer_wallet_id' => $data['payer'],
-                'payee_wallet_id' => $data['payee'],
-                'value' => $data['value'],
-                'processed' => false
-            ]
-        );
+        return [
+            'id' => $transaction_id,
+            'payer_wallet_id' => $transaction['payer'],
+            'payee_wallet_id' => $transaction['payee'],
+            'value' => $transaction['value'],
+            'processed' => false
+        ];
+    }
 
-        dispatch(new ProcessTransactionJob($transaction))
-            ->onQueue('transactionJobQueue');
+    public function payerWalletHasEnoughBalance(string $payer_wallet_id, $value): bool
+    {
+        $payer_wallet = $this->walletRepository->findOneBy($payer_wallet_id);
+        return $payer_wallet['balance'] >= $value;
+    }
 
-        event(new NewTransactionCreatedEvent($transaction));
+    public function payerIsACommonUser(string $payer_wallet_id): bool
+    {
+        $payer_wallet = $this->walletRepository->findOneBy($payer_wallet_id);
+        $payer = $this->userRepository->findOneBy($payer_wallet['user_id']);
+        return !$payer['shopkeeper'];
+    }
 
-        return $transaction;
+    public function isTransactionAuthorized(array $transaction): bool
+    {
+        return $this->externalAuthorizerService->authorize($transaction);
     }
 }
