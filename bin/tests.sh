e27df00abf21b7a23d228523e3d9cd822a4fc140
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-07-16 22:51:07 -0300

    tests: add phpmd in tests suit

--- bin/tests.sh
+++ bin/tests.sh
@@ -8,16 +8,28 @@ echo ""
 echo "---------------------------------------------"
 echo "                   PHPUnit"
 echo "---------------------------------------------"
-docker-compose exec php ./vendor/bin/phpunit --do-not-cache-result
+docker-compose exec php ./vendor/bin/phpunit --do-not-cache-result && echo "" && echo "PHPStan passed"
 
 echo ""
 echo "---------------------------------------------"
 echo "                   PHPCS"
 echo "---------------------------------------------"
-docker-compose exec php ./vendor/bin/phpcs
+docker-compose exec php ./vendor/bin/phpcs && echo "" && echo "PHPStan passed"
 
 echo ""
 echo "---------------------------------------------"
 echo "                   PHPStan"
 echo "---------------------------------------------"
-docker-compose exec php ./vendor/bin/phpstan analyse  --memory-limit=2G
+docker-compose exec php ./vendor/bin/phpstan analyse  --memory-limit=2G && echo "" && echo "PHPStan passed"
+
+echo ""
+echo "---------------------------------------------"
+echo "                   PHPMD"
+echo "---------------------------------------------"
+docker-compose exec php ./vendor/bin/phpmd app text cleancode,codesize,controversial,design,naming,unusedcode && echo "" && echo "PHPMD passed"
+
+echo ""
+echo "---------------------------------------------"
+echo "                   ALL TESTS PASSED"
+echo "---------------------------------------------"
+
