caa65dba0dfb2aafb30a0789e879eaa1cdd70ffb
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-07-18 17:16:34 -0300

    tests: add user tests

---
+++ tests/Integration/App/Http/Controllers/UserControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\User;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class UserControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('db:seed');
+    }
+
+    public function test_can_list_all_users(): void
+    {
+        $contentJson = $this->get("v1/users")->response->getContent();
+        $content = json_decode($contentJson);
+
+        $this->seeStatusCode(206);
+        $this->seeJsonStructure(['data' => 'data']);
+        $this->assertCount(4, $content->data->data);
+    }
+
+    public function test_can_list_an_specific_user(): void
+    {
+        $user = User::factory()->create();
+
+        $this->get("v1/users/{$user->id}");
+
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(['data' => ['id', 'email']]);
+        $this->seeInDatabase('users', [
+            'id' => $user->id,
+            'email' => $user->email
+        ]);
+    }
+}
