513cb4ceb4dbea2149f752d213cc789bc7115b84
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-08-04 21:52:30 -0300

    refactor: pure sql with db inserts on migrations

--- database/migrations/2021_07_13_031658_create_users_table.php
+++ database/migrations/2021_07_13_031658_create_users_table.php
@@ -2,10 +2,46 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
+    const USERS = [
+        [
+            'id' => '5d541b1b-e038-41dd-96d1-8b757b4d23f0',
+            'full_name' => 'John Doe',
+            'email' => 'john.doe@test.com',
+            'password' => '$2y$10$IiPnvo1IcavNTDUCeeTK7OEj8lZm65eedj2/A0dgvwm67LBK3onAa',
+            'registration_number' => '76997937063',
+            'shopkeeper' => 0,
+        ],
+        [
+            'id' => '5f97a1d3-a2e2-4a58-aa48-66cd49ab14a8',
+            'full_name' => 'Bob Dylan',
+            'email' => 'bob.dylan@test.com',
+            'password' => '$2y$10$IiPnvo1IcavNTDUCeeTK7OEj8lZm65eedj2/A0dgvwm67LBK3onAa',
+            'registration_number' => '08087648021',
+            'shopkeeper' => 0
+        ],
+        [
+            'id' => '1084fe32-5a84-4ce4-8ec0-3cbbe4485863',
+            'full_name' => 'John Dealer',
+            'email' => 'john.dealer@test.com',
+            'password' => '$2y$10$IiPnvo1IcavNTDUCeeTK7OEj8lZm65eedj2/A0dgvwm67LBK3onAa',
+            'registration_number' => '39382854000115',
+            'shopkeeper' => 1
+        ],
+        [
+            'id' => '92ffc998-5e2a-488f-ba43-76f0467c7f6f',
+            'full_name' => 'Bob Dealer',
+            'email' => 'bob.dealer@test.com',
+            'password' => '$2y$10$IiPnvo1IcavNTDUCeeTK7OEj8lZm65eedj2/A0dgvwm67LBK3onAa',
+            'registration_number' => '64041937000198',
+            'shopkeeper' => 1
+        ],
+    ];
+
     /**
      * Run the migrations.
      *
@@ -23,6 +59,11 @@ class CreateUsersTable extends Migration
             $table->softDeletes();
             $table->timestamps();
         });
+
+        if (app()->environment('local')) {
+            // To populate db only once
+            DB::table('users')->insert(self::USERS);
+        }
     }
 
     /**
--- database/migrations/2021_07_13_031937_create_wallets_table.php
+++ database/migrations/2021_07_13_031937_create_wallets_table.php
@@ -2,10 +2,34 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateWalletsTable extends Migration
 {
+    const WALLETS = [
+        [
+            'id' => '91e92c5f-d9d0-437a-9435-58839fdbb6c5',
+            'user_id' => '5d541b1b-e038-41dd-96d1-8b757b4d23f0',
+            'balance' => 1000
+        ],
+        [
+            'id' => 'b786b829-2a9a-4454-af52-b06a552d845c',
+            'user_id' => '5f97a1d3-a2e2-4a58-aa48-66cd49ab14a8',
+            'balance' => 100
+        ],
+        [
+            'id' => '9442fd46-44cf-4571-9bfd-59670b765719',
+            'user_id' => '1084fe32-5a84-4ce4-8ec0-3cbbe4485863',
+            'balance' => 10000
+        ],
+        [
+            'id' => '90e75b1a-ae80-4c28-b1c8-7f06d35e7f60',
+            'user_id' => '92ffc998-5e2a-488f-ba43-76f0467c7f6f',
+            'balance' => 50000
+        ],
+    ];
+
     /**
      * Run the migrations.
      *
@@ -19,6 +43,11 @@ class CreateWalletsTable extends Migration
             $table->float('balance')->define(0);
             $table->timestamps();
         });
+
+        if (app()->environment('local')) {
+            // To populate db only once
+            DB::table('wallets')->insert(self::WALLETS);
+        }
     }
 
     /**
